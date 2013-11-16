3b40e33bbbb8e00b28cccccfe09c03fa4fa5bab3
Uli Fuchs <android-kennenlernen@gmx.de>
2013-11-16 22:49:29 +0100

    Handler eingehangen

--- src/com/example/rechner/MainActivity.java
+++ src/com/example/rechner/MainActivity.java
@@ -3,21 +3,140 @@ package com.example.rechner;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private String displayText;
+	
+	private EditText etDisplay;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        this.etDisplay = (EditText) findViewById(R.id.etDisplay);
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    /**
+     * Liest Display aus
+     * @return Zahl im Display
+     */
+    String readDisplay() {
+    	return this.etDisplay.getText().toString();
+    }
+
+    /**
+     * Schreibt auf's Display 
+     * @param value - Darzustellende Zahl
+     */
+    void writeDisplay(String value) {
+    	this.etDisplay.setText(value);
+    }     
+    
+    /**
+     * 
+     * @param value
+     * @return
+     */
+    boolean hasPeriod(String value) {
+    	return value.contains(".");
+    }
+    
+    /**
+     * 
+     * @param value - Die zu invertierende Zahl als Zeichenkette 
+     * @return Die invertierte Zahl als Zeichenkette
+     */
+    String invertNumber(String value) {
+    	
+    	int len = value.length();
+    	
+    	int periodPos = value.lastIndexOf('.');
+    	
+    	boolean trueDouble = this.hasPeriod(value); 
+//    			&& (len == periodPos);
+
+    	if (trueDouble) {
+        	double res = Double.parseDouble(value) * (-1.0);
+        	return Double.toString(res);
+    	} else {
+    		int res = Integer.parseInt(value) * -1;
+    		return Integer.toString(res);
+    	}
+    	
+    }     
+    
+    /**
+     * 
+     * @param v
+     */
+    public void onClickBtnNum(View v) {
+    	
+
+    	String num = "";
+    	
+    	int id = v.getId();
+    	
+    	this.displayText = this.readDisplay();
+    	
+    	switch (id) {
+    	
+    	case R.id.btnNum_0 : num = "0"; break;
+    	case R.id.btnNum_1 : num = "1"; break; 
+    	case R.id.btnNum_2 : num = "2"; break;
+    	case R.id.btnNum_3 : num = "3"; break;
+    	case R.id.btnNum_4 : num = "4"; break;
+    	case R.id.btnNum_5 : num = "5"; break;
+    	case R.id.btnNum_6 : num = "6"; break;
+    	case R.id.btnNum_7 : num = "7"; break;
+    	case R.id.btnNum_8 : num = "8"; break;
+    	case R.id.btnNum_9 : num = "9"; break;
+    	case R.id.btnNum_Period : num = !this.hasPeriod(this.displayText) ? "." : ""; 
+    		break;
+    	case R.id.btnNum_Invert : this.displayText = invertNumber(this.displayText); 
+    		break;    		
+    	}     	
+ 
+    	this.displayText += num;
+    	
+    	this.writeDisplay(this.displayText);
+    		
+    		
+    }
+
+    /**
+     * 
+     */
+    void clearOp() {
+    	this.displayText = "";
+    	this.writeDisplay(this.displayText);
+    }
+    
+    /**
+     * 
+     * @param v
+     */
+    public void onClickBtnOp(View v) {
+
+    	int id = v.getId();
+    	
+    	switch (id) {
+    	
+    	case R.id.btnOp_Clear : clearOp();break;
+    	
+    	}     	
+    	
     }
     
 }
