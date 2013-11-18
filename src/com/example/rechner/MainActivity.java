080df7c673c8e373f709b5a99f229d15755ef65c
Uli Fuchs <android.kennenlernen@gmx.de>
2013-11-18 17:57:48 +0100

    Operator changed  to Operation

--- src/com/example/rechner/MainActivity.java
+++ src/com/example/rechner/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.widget.EditText;
 
 // Art der gewuenschten Operation
-enum Operator {none, add, sub, mul, div};
+enum Operation {none, add, sub, mul, div};
 
 // Zustand des Rechenwerkes
 enum State {clean, hasOp1, hasOp2}; 
